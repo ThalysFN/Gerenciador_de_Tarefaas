22bdc088e41eef68031c43b4e486dd4c253cf52b
Schmegel09 <gabriel.schmegel.23@hotmail.com>
2023-06-18 10:27:05 -0300

    Revert "Merge branch 'main' of https://github.com/ThalysFN/Gerenciador_de_Tarefaas"
    This reverts commit c29118bda768391e7613e3bda7b884354f9d414c, reversing
    changes made to 0bc250cf6fb4998e7c46c9acd0d56ddddce5be8f.

--- teste.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
-
-const privateKeyBase64 = Buffer.from(process.env.PRIVATE_KEY, 'UTF-8').toString('base64');
-
-console.log(privateKeyBase64); // Exibe a chave privada codificada em base64
